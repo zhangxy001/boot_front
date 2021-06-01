5abd4d0738c93e0760bb7500e300f2312cd514e5
zhangxinyu001 <915286304@qq.com>
2021-06-01 17:49:18 +0800

    第二版开始。。

--- sys/js/common.js
+++ sys/js/common.js
@@ -1,1 +1,9 @@
 var baseUrl = "http://localhost:8080/medical/";
+
+//var baseUrl = "http://49.232.83.75/medical/"
+function help(){
+    alert("需要帮助，请联系管理员。官方微信 zxy915286304");
+}
+function exportTemplate(){
+    window.location.href=baseUrl+"export/exportFile";
+}
