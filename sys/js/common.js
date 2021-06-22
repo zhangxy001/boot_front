afaf09af9d0c08f4e2bde797dba8c8c8985f7419
zhangxinyu001 <915286304@qq.com>
2021-06-22 17:33:48 +0800

    代码为什么提不上去，垃圾软件

--- sys/js/common.js
+++ sys/js/common.js
@@ -1,9 +1,24 @@
 var baseUrl = "http://localhost:8080/medical/";
 
 //var baseUrl = "http://49.232.83.75/medical/"
+const userId = window.localStorage.getItem("userId");
+const userName = window.localStorage.getItem("name");
+const userRole = window.localStorage.getItem("role");
 function help(){
     alert("需要帮助，请联系管理员。官方微信 zxy915286304");
 }
-function exportTemplate(){
-    window.location.href=baseUrl+"export/exportFile";
+function exportTemplate(id){
+    var url = "export/exportFile";
+    if(id == "2"){
+        url = "export/exportFile2";
+    }
+    if(id == "3"){
+        url = "export/exportFile3";
+    }
+    if(id == "4"){
+        url = "export/exportFile4";
+    }
+    // alert (url);
+    // return;
+    window.location.href=baseUrl+url;
 }
